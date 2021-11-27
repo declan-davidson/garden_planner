e6636ab900118449e7223bade2a18ce668ad96ab
Declan <2038796D@student.gla.ac.uk>
2021-11-27 15:13:54 +0000

    Initial expansion solution

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
   static const List<Tab> _tabs = [Tab(text: "My Garden"), Tab(text: "Exchange")];
-  List<String> fabLabels = ["Create", "List"];
+  List<CardDefinition> cards = [
+    CardDefinition("Harvest", "Next up: {crop}", "harvest.jpg", 1),
+    CardDefinition("Plan", "freeArea}m2 available", "plan.jpg", 2),
+    CardDefinition("Recipes", "Why not try {dish}?", "recipe.jpg", 3),
+    CardDefinition("Environmental Impact", "How's my carbon footprint?", "env_imp.jpg", 4)];
   late TabController _tabController;
   int _index = 0;
 
@@ -59,11 +63,42 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
       body: TabBarView(
         controller: _tabController,
         children: [
-          SizedBox(
+          Container(
+            margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+            child: ListView.builder(
+              itemCount: cards.length,
+              itemBuilder: (BuildContext c, int i) {
+                return GestureDetector(
+                  child: SizedBox(
+                    height: 200,
+                    child: Card(
+                      color: Colors.white,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Hero(
+                            tag: "${cards[i].id}",
+                            child: Image.asset(cards[i].imagePath, height: 128, width: double.infinity, fit: BoxFit.fitWidth)
+                          ),
+                          ListTile(
+                            title: Text(cards[i].title, style: TextStyle(fontWeight: FontWeight.bold),), 
+                            subtitle: Text(cards[i].subtitle)
+                          )
+                        ],
+                      )
+                    )
+                  ),
+                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => ExpandedCard(cards[i]))),
+                );
+              }
+            )
+          ),
+          /* SizedBox(
             height: 1000,
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: ListView(
+              child: 
+              /* child: ListView(
                 children: [
                   const Padding(
                     padding: EdgeInsets.only(top: 10)
@@ -136,9 +171,9 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
                     padding: EdgeInsets.only(bottom: 10)
                   ),
                 ]
-              )
+              ) */
             )
-          ),
+          ), */
           Scaffold(
             floatingActionButton: FloatingActionButton.extended(
               label: const Text("List"),
@@ -147,6 +182,56 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
             ),
           )
         ]
+      )
+    );
+  }
+}
+
+class CardDefinition {
+  String title, subtitle, imagePath;
+  int id;
+
+  CardDefinition(this.title, this.subtitle, this.imagePath, this.id);
+}
+
+class ExpandedCard extends StatelessWidget {
+  CardDefinition card;
+
+  ExpandedCard(this.card);
+
+  @override
+  Widget build(BuildContext context){
+    //A structural widget that displays full screen minus OS items like the status bar
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0
+        ),
+        extendBodyBehindAppBar: true, //Body becomes full screen
+        body: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Hero(
+                tag: "${card.id}",
+                child: Image.asset(card.imagePath, scale: 1.0, repeat: ImageRepeat.noRepeat, fit: BoxFit.fitWidth, height: MediaQuery.of(context).size.height / 5)
+              ),
+              ListTile(
+                title: Text(card.title, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                subtitle: Text(card.subtitle)
+              ),
+              const Divider(
+                height: 20,
+                thickness: 1
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text("Some details here", style: TextStyle(fontSize: 25))
+              )
+            ]
+          )
+        )
       )
     );
   }
