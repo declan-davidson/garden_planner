90c16055c8d89aa80f3af372350e7e7e94f6044d
Craig Murray <craig.murray095@gmail.com>
2021-11-29 15:24:58 +0000

    Added map API settings

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,13 +8,19 @@ class HomePage extends StatefulWidget {
   State<StatefulWidget> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
-  static const List<Tab> _tabs = [Tab(text: "My Garden"), Tab(text: "Exchange")];
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  static const List<Tab> _tabs = [
+    Tab(text: "My Garden"),
+    Tab(text: "Exchange")
+  ];
   List<CardDefinition> cards = [
     CardDefinition("Harvest", "Next up: {crop}", "harvest.jpg", 1, true),
     CardDefinition("Plan", "freeArea}m2 available", "plan.jpg", 2, false),
     CardDefinition("Recipes", "Why not try {dish}?", "recipe.jpg", 3, true),
-    CardDefinition("Environmental Impact", "How's my carbon footprint?", "env_imp.jpg", 4, false)];
+    CardDefinition("Environmental Impact", "How's my carbon footprint?",
+        "env_imp.jpg", 4, false)
+  ];
   late TabController _tabController;
 
   @override
@@ -26,39 +32,30 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Garden Planner"),
-        backgroundColor: Colors.green,
-        foregroundColor: Colors.white,
-        bottom: TabBar(
-          controller: _tabController,
-          labelColor: Colors.white,
-          unselectedLabelColor: Colors.white,
-          indicatorColor: Colors.white,
-          tabs: _tabs
-        )
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: [
+        appBar: AppBar(
+            title: const Text("Garden Planner"),
+            backgroundColor: Colors.green,
+            foregroundColor: Colors.white,
+            bottom: TabBar(
+                controller: _tabController,
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.white,
+                indicatorColor: Colors.white,
+                tabs: _tabs)),
+        body: TabBarView(controller: _tabController, children: [
           Container(
-            margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-            child: ListView.builder(
-              itemCount: cards.length,
-              itemBuilder: (BuildContext c, int i) {
-                return createTappableCard(context, cards[i]);
-              }
-            )
-          ),
+              margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: ListView.builder(
+                  itemCount: cards.length,
+                  itemBuilder: (BuildContext c, int i) {
+                    return createTappableCard(context, cards[i]);
+                  })),
           Scaffold(
             floatingActionButton: FloatingActionButton.extended(
-              label: const Text("List"),
-              icon: const Icon(Icons.add_sharp),
-              onPressed: () => {}
-            ),
+                label: const Text("List"),
+                icon: const Icon(Icons.add_sharp),
+                onPressed: () => {}),
           )
-        ]
-      )
-    );
+        ]));
   }
 }
