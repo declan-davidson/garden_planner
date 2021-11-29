1541c0921280b6b2b175397ad5880e0d23c96293
Declan <2038796D@student.gla.ac.uk>
2021-11-29 14:41:37 +0000

    Added placeholder plan page for toggling carrot planting; further card modifications

--- lib/card.dart
+++ lib/card.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
+import 'plan.dart';
+
+class ExpandedCardListItem {
+  late Text title, subtitle;
+  late Icon icon;
+
+  ExpandedCardListItem(String title, String subtitle, String iconPath){
+    this.title = Text(title);
+    this.subtitle = Text(subtitle);
+    icon = const Icon(Icons.agriculture_sharp);
+  }
+}
+
+List<ExpandedCardListItem> noCarrotHarvestItems = [
+  ExpandedCardListItem("Potatoes", "Harvest on June 1st", "somePath")
+];
+
+List<ExpandedCardListItem> noCarrotRecipeItems = [
+  ExpandedCardListItem("Dauphinoise potatoes", "Requires potatoes", "somePath"),
+  ExpandedCardListItem("Potato and leek soup", "Requires potatoes, leeks", "somePath")
+];
+
+List<ExpandedCardListItem> carrotHarvestItems = [
+  ExpandedCardListItem("Carrots", "Harvest on May 15th", "somePath")
+] + noCarrotHarvestItems;
+
+List<ExpandedCardListItem> carrotRecipeItems = [
+  ExpandedCardListItem("Carrot cake", "Requires carrots", "somePath"),
+  ExpandedCardListItem("Chicken casserole", "Requires carrots, potatoes", "somePath")
+] + noCarrotRecipeItems;
 
 class CardDefinition {
-  String title, subtitle, imagePath;
-  int id;
+  String title, subtitle, imagePath, id;
   bool expandable;
 
   CardDefinition(this.title, this.subtitle, this.imagePath, this.id, this.expandable);
@@ -11,11 +40,30 @@ class CardDefinition {
 
 class ExpandedCard extends StatelessWidget {
   final CardDefinition card;
+  bool carrotsPlanted;
 
-  ExpandedCard(this.card, {Key? key}) : super(key: key);
+  ExpandedCard(this.card, this.carrotsPlanted, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context){
+    List<ExpandedCardListItem> listItems = [];
+    if(card.id == "harvest"){
+      if(carrotsPlanted){
+        listItems = carrotHarvestItems;
+      }
+      else{
+        listItems = noCarrotHarvestItems;
+      }
+    }
+    else if(card.id == "recipes"){
+      if(carrotsPlanted){
+        listItems = carrotRecipeItems;
+      }
+      else{
+        listItems = noCarrotRecipeItems;
+      }
+    }
+
     //A structural widget that displays full screen minus OS items like the status bar
     return SafeArea(
       child: Scaffold(
@@ -29,7 +77,7 @@ class ExpandedCard extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Hero(
-                tag: "${card.id}",
+                tag: card.id,
                 child: Image.asset(card.imagePath, scale: 1.0, repeat: ImageRepeat.noRepeat, fit: BoxFit.fitWidth, height: MediaQuery.of(context).size.height / 5)
               ),
               ListTile(
@@ -40,9 +88,20 @@ class ExpandedCard extends StatelessWidget {
                 height: 20,
                 thickness: 1
               ),
-              const Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Text("Some details here", style: TextStyle(fontSize: 25))
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: ListView.builder(
+                  padding: EdgeInsets.zero,
+                  shrinkWrap: true,
+                  itemCount: listItems.length,
+                  itemBuilder: (BuildContext c, int i) {
+                    return ListTile(
+                      title: listItems[i].title,
+                      subtitle: listItems[i].subtitle,
+                      leading: listItems[i].icon
+                    );
+                  }
+                )
               )
             ]
           )
@@ -52,8 +111,9 @@ class ExpandedCard extends StatelessWidget {
   }
 }
 
-GestureDetector createTappableCard(BuildContext context, CardDefinition card) {
+GestureDetector createTappableCard(BuildContext context, CardDefinition card, {bool Function()? getterCallback, VoidCallback? togglerCallback}) {
   var onTap = () => Navigator.push(context, PageTransition(child: _PlaceHolderPage(), type: PageTransitionType.rightToLeft, duration: Duration(milliseconds: 200), reverseDuration: Duration(milliseconds: 200)));
+
   List<Widget> cardBody = [
     Image.asset(card.imagePath, height: 128, width: double.infinity, fit: BoxFit.fitWidth),
     ListTile(
@@ -63,16 +123,21 @@ GestureDetector createTappableCard(BuildContext context, CardDefinition card) {
   ];
 
   if(card.expandable){
-    onTap = () => Navigator.push(context, MaterialPageRoute(builder: (context) => ExpandedCard(card)));
+    onTap = () => Navigator.push(context, MaterialPageRoute(builder: (context) => ExpandedCard(card, (getterCallback != null ? getterCallback() : false))));
 
     //Have card use Hero transition
     cardBody[0] = Hero(
-        tag: "${card.id}",
+        tag: card.id,
         child: Image.asset(card.imagePath, height: 128, width: double.infinity, fit: BoxFit.fitWidth)
     );
   }
 
-  return GestureDetector(
+  if(card.id == "plan"){
+    onTap = () => Navigator.push(context, PageTransition(child: PlanPage(getterCallback != null ? getterCallback : () => false ,togglerCallback != null ? togglerCallback : () => {}), type: PageTransitionType.rightToLeft, duration: Duration(milliseconds: 200), reverseDuration: Duration(milliseconds: 200)));
+  }
+
+  //Callback prop is probably not needed; remove later
+  return TappableCard(
     child: SizedBox(
       height: 200,
       child: Card(
@@ -83,12 +148,19 @@ GestureDetector createTappableCard(BuildContext context, CardDefinition card) {
         )
       )
     ),
-    onTap: onTap
+    onTap: onTap,
+    callback: getterCallback,
   );
 }
 
+class TappableCard extends GestureDetector{
+  Function? callback;
+
+  TappableCard({Widget? child, var onTap, Key? key, this.callback}) : super(key: key, child: child, onTap: onTap);
+}
+
 class _PlaceHolderPage extends StatelessWidget{
-  const _PlaceHolderPage({Key? key}) : super(key: key);
+  _PlaceHolderPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context){
@@ -97,7 +169,8 @@ class _PlaceHolderPage extends StatelessWidget{
         title: const Text("Placeholder"),
         backgroundColor: Colors.green,
         foregroundColor: Colors.white,
-      )
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () => {})
     );
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,16 +11,25 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
   static const List<Tab> _tabs = [Tab(text: "My Garden"), Tab(text: "Exchange")];
   List<CardDefinition> cards = [
-    CardDefinition("Harvest", "Next up: {crop}", "harvest.jpg", 1, true),
-    CardDefinition("Plan", "freeArea}m2 available", "plan.jpg", 2, false),
-    CardDefinition("Recipes", "Why not try {dish}?", "recipe.jpg", 3, true),
-    CardDefinition("Environmental Impact", "How's my carbon footprint?", "env_imp.jpg", 4, false)];
+    CardDefinition("Harvest", "Next up: {crop}", "harvest.jpg", "harvest", true),
+    CardDefinition("Plan", "freeArea}m2 available", "plan.jpg", "plan", false),
+    CardDefinition("Recipes", "Why not try {dish}?", "recipe.jpg", "recipes", true),
+    CardDefinition("Environmental Impact", "How's my carbon footprint?", "env_imp.jpg", "environmental_impact", false)];
   late TabController _tabController;
+  bool _carrotsPlanted = false;
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(vsync: this, length: _tabs.length);
+  }
+
+  bool getCarrotsPlanted(){
+    return _carrotsPlanted;
+  }
+
+  void toggleCarrotsPlanted(){
+    _carrotsPlanted = !_carrotsPlanted;
   }
 
   @override
@@ -46,7 +55,7 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
             child: ListView.builder(
               itemCount: cards.length,
               itemBuilder: (BuildContext c, int i) {
-                return createTappableCard(context, cards[i]);
+                return createTappableCard(context, cards[i], getterCallback: getCarrotsPlanted, togglerCallback: toggleCarrotsPlanted);
               }
             )
           ),
