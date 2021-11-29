f2c4b688d8961f58bc70615a3cc05df456ae521a
Declan <2038796D@student.gla.ac.uk>
2021-11-29 18:50:59 +0000

    Initial exchange page solution

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'card.dart';
-//import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'exchange.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -25,34 +24,6 @@ class _HomePageState extends State<HomePage>
   ];
 
   late TabController _tabController;
-  late GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(55.860916, -4.251433);
-  final Map<String, Marker> _markers = {};
-
-  Future<void> _onMapCreated(GoogleMapController controller) async {
-    setState(() {
-      _markers.clear();
-      const marker1 = Marker(
-        markerId: MarkerId("Garden_1"),
-        position: LatLng(55.8724, -4.2900),
-        infoWindow: InfoWindow(
-          title: "West End Garden",
-          snippet: "Looking to exchange crops!",
-        ),
-      );
-      const marker2 = Marker(
-        markerId: MarkerId("Garden_2"),
-        position: LatLng(55.85111, -4.23722),
-        infoWindow: InfoWindow(
-          title: "East End Garden",
-          snippet: "Looking to exchange crops!",
-        ),
-      );
-      _markers["garden_1"] = marker1;
-      _markers["garden_2"] = marker2;
-    });
-  }
 
   @override
   void initState() {
@@ -81,16 +52,7 @@ class _HomePageState extends State<HomePage>
                   itemBuilder: (BuildContext c, int i) {
                     return createTappableCard(context, cards[i]);
                   })),
-          Scaffold(
-            body: GoogleMap(
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: _center,
-                zoom: 11.0,
-              ),
-              markers: _markers.values.toSet(),
-            ),
-          ),
+          Exchange()
         ]));
   }
 }
